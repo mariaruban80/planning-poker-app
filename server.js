2597638b333acff276845e3a78b95c77f1a57be1
mariaruban80 <maria.ruban80@gmail.com>
2025-04-26 18:44:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,84 +1,94 @@
-// server.js (CommonJS version)
-
 const express = require('express');
-const http = require('http');
+const { createServer } = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
 
 const app = express();
-const server = http.createServer(app);
+const server = createServer(app);
 const io = new Server(server);
 
-const rooms = {};
+const rooms = {}; // { roomId: { users: [], votes: {}, story: "" } }
 
-// Serve static files from public folder
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route handling for all paths to serve index.html (for room-based URLs)
+// Serve index.html for any GET request
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// WebSocket logic
+// WebSocket handling
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('New client connected:', socket.id);
 
   socket.on('join', ({ roomId, user }) => {
     socket.join(roomId);
     if (!rooms[roomId]) {
-      rooms[roomId] = { users: [], votes: {}, story: null };
+      rooms[roomId] = { users: [], votes: {}, story: '' };
     }
     rooms[roomId].users.push(user);
+
     io.to(roomId).emit('userList', { users: rooms[roomId].users });
+    console.log(`User ${user} joined room ${roomId}`);
   });
 
   socket.on('vote', ({ story, vote }) => {
-    const roomId = [...socket.rooms][1];
-    if (rooms[roomId]) {
-      rooms[roomId].votes[socket.id] = vote;
-      io.to(roomId).emit('voteUpdate', { story, votes: rooms[roomId].votes });
+    const roomId = getUserRoom(socket);
+    if (roomId && rooms[roomId]) {
+      if (!rooms[roomId].votes[story]) {
+        rooms[roomId].votes[story] = {};
+      }
+      rooms[roomId].votes[story][socket.id] = vote;
+      io.to(roomId).emit('voteUpdate', { story, votes: rooms[roomId].votes[story] });
     }
   });
 
   socket.on('storyChange', ({ story }) => {
-    const roomId = [...socket.rooms][1];
-    if (rooms[roomId]) {
+    const roomId = getUserRoom(socket);
+    if (roomId && rooms[roomId]) {
       rooms[roomId].story = story;
       io.to(roomId).emit('storyChange', { story });
     }
   });
 
   socket.on('revealVotes', () => {
-    const roomId = [...socket.rooms][1];
-    if (rooms[roomId]) {
+    const roomId = getUserRoom(socket);
+    if (roomId) {
       io.to(roomId).emit('revealVotes');
     }
   });
 
   socket.on('resetVotes', () => {
-    const roomId = [...socket.rooms][1];
-    if (rooms[roomId]) {
+    const roomId = getUserRoom(socket);
+    if (roomId && rooms[roomId]) {
       rooms[roomId].votes = {};
       io.to(roomId).emit('resetVotes');
     }
   });
 
   socket.on('disconnect', () => {
-    console.log('User disconnected');
-    for (const roomId in rooms) {
-      const room = rooms[roomId];
-      room.users = room.users.filter(user => user !== socket.id);
-      if (room.users.length === 0) {
-        delete rooms[roomId];
-      } else {
-        io.to(roomId).emit('userList', { users: room.users });
-      }
+    const roomId = getUserRoom(socket);
+    if (roomId && rooms[roomId]) {
+      // Remove user from room
+      rooms[roomId].users = rooms[roomId].users.filter(u => u !== socket.userName);
+      io.to(roomId).emit('userList', { users: rooms[roomId].users });
     }
+    console.log('Client disconnected:', socket.id);
+  });
+
+  socket.on('ping', () => {
+    socket.emit('pong');
   });
 });
 
-// Start the server
+// Helper function
+function getUserRoom(socket) {
+  const roomsJoined = Array.from(socket.rooms).filter(r => r !== socket.id);
+  return roomsJoined[0];
+}
+
+// Start server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
