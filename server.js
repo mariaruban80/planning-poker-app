29f1328be653083adce7ff877068fd4538fb8595
mariaruban80 <maria.ruban80@gmail.com>
2025-04-29 21:32:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ io.on('connection', (socket) => {
       //rooms[currentRoom] = { users: [], votes: {}, story: [], revealed: false };
     }
 
+    
     rooms[currentRoom].users.push({ id: socket.id, name: userName });
 
     socket.join(currentRoom);
@@ -49,8 +50,9 @@ io.on('connection', (socket) => {
     io.to(currentRoom).emit('userList', rooms[currentRoom].users);
 
     // Send current CSV data to the new user if available
-    if (rooms[currentRoom].story && rooms[currentRoom].story.length > 0){
+   // if (rooms[currentRoom].story && rooms[currentRoom].story.length > 0){
     //if (rooms[currentRoom].csvData.length > 0) {
+      if (rooms[currentRoom].csvData && rooms[currentRoom].csvData.length > 0) {
       socket.emit('syncCSVData', rooms[currentRoom].csvData);
     }
   });
